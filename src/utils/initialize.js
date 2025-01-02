785fa85504783ac6568a2c5c76c18e740b157842
Naim Ayoub <127954922+NaiM-AyouB@users.noreply.github.com>
2025-01-02 23:42:32 +0100

    src_utils direct (theme + projects +normalise = initialize)

---
+++ src/utils/initialize.js
@@ -0,0 +1,15 @@
+import AOS from 'aos';
+
+
+function initialize() {
+    // aos initialize
+    AOS.init({
+        duration: 500, // values from 0 to 3000, with step 50ms
+        easing: 'ease', // default easing for AOS animations
+        offset: window.innerHeight / 2, // animate after element get in midlle of screen
+        once: true, // whether animation should happen only once - while scrolling down
+        delay: 0, // no delay
+    });
+}
+
+export default initialize
---
+++ src/utils/projects.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		image: "/assets/projects preview/project.png",
+		title: "Project 1",
+		technologies: ["Sass", "ReactJs", "Vite"],
+		link: "",
+	},
+	{
+		image: "/assets/projects preview/project.png",
+		title: "Project 2",
+		technologies: ["ReactRouter", "ReactJs", "Vite"],
+		link: "",
+	},
+	{
+		image: "/assets/projects preview/project.png",
+		title: "Project 3",
+		technologies: ["ReactRouter", "ReactJs", "Vite"],
+		link: "",
+	},
+	{
+		image: "/assets/projects preview/project.png",
+		title: "Project 4",
+		technologies: ["css", "html", "Js"],
+		link: "",
+	},
+	{
+		image: "/assets/projects preview/project.png",
+		title: "Project 5",
+		technologies: ["react", "Chakra ui", "React Router"],
+		link: "",
+	},
+];
